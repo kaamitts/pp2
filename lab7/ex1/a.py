57e9abb305484d0aec55747e439200d14c94c1e6
Kanatbek <atamuratovkanatbek@gmail.com>
2024-03-16 14:18:57 +0500

    Seventh commit

--- lab7/ex1/a.py
+++ lab7/ex1/a.py
@@ -1,49 +1,37 @@
 import pygame
-import os
-import time
+import datetime
 
 pygame.init()
-screen = pygame.display.set_mode((830, 800))
-done = False
+W, H = 800, 800
+x = W // 2
+y = H // 2
+WHITE = (255, 255, 255)
+sc = pygame.display.set_mode((W, H))
 clock = pygame.time.Clock()
+mickey = pygame.image.load("mainclock1.png")
+leftHand = pygame.image.load("leftarm.png")
+rightHand = pygame.image.load("rightarm1.png")
+mickeyRect = mickey.get_rect()
 
-# Функция загрузки изображений
-def get_image(path):
-    canonical_path = path.replace("\\", os.sep)
-    return pygame.image.load(canonical_path)
+def blitRotateCenter(surf, image, center, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center=image.get_rect(center=center).center)
+    surf.blit(rotated_image, new_rect)
 
-# Загрузка изображений для часов и рук Микки
-clock_image = get_image("mainclock1.png")
-left_arm_image = get_image("leftarm.png")
-right_arm_image = get_image("rightarm.png")
-
-# Главный цикл приложения
-while not done:
+while True:
+    sc.fill(0)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
-    
-    # Очистка экрана
-    screen.fill((255,255,255)) 
-    
-    # Отображение часов на экране
-    screen.blit(clock_image, (0, 0))
-    
-    # Получение текущего времени
-    current_time = time.localtime()
-    minutes_angle = current_time.tm_min * 6  # 360 градусов / 60 минут = 6 градусов на минуту
-    seconds_angle = current_time.tm_sec * 6  # 360 градусов / 60 секунд = 6 градусов на секунду
-    
-    # Поворот правой руки Микки в зависимости от минут
-    rotated_right_arm = pygame.transform.rotate(right_arm_image, -minutes_angle)
-    screen.blit(rotated_right_arm, (394, -85))
-    
-    # Поворот левой руки Микки в зависимости от секунд
-    rotated_left_arm = pygame.transform.rotate(left_arm_image, -seconds_angle)
-    screen.blit(rotated_left_arm, (394, -85))
-    
-    # Обновление экрана
-    pygame.display.flip()
-    
-    # Ограничение частоты кадров
+            exit()
+
+    current_time = datetime.datetime.now()
+    hour_angle = -(current_time.minute * 6)  # Угол для часовой стрелки (зависит от минут)
+    minute_angle = -(current_time.second * 6)  # Угол для минутной стрелки (зависит от секунд)
+
+    sc.fill(WHITE)
+    sc.blit(mickey, (0, 0))
+    blitRotateCenter(sc, leftHand, (x, y), minute_angle)
+    blitRotateCenter(sc, rightHand, (x, y), hour_angle)
+
+    pygame.display.update()
     clock.tick(60)
--- lab7/ex1/ex1.py
+++ lab7/ex1/ex1.py
@@ -1,46 +1,37 @@
-import pygame, os, time
-
+import pygame
+import datetime
 pygame.init()
-screen = pygame.display.set_mode((830, 800))
-done = False
-x = 200
-y = 200
+W, H = 800, 800
+x = W//2
+y = H//2 
+WHITE = (255, 255, 255)
+
+screen = pygame.display.set_mode((800,800))
 clock = pygame.time.Clock()
-angle = 20
-_image_librery = {}
-def get_image(path):
-    global _image_librery
-    image =_image_librery.get(path)
-    if image == None:
-        canon_path = path.replace("\\", os.sep)
-        image = pygame.image.load(canon_path)
-        _image_librery[path] = image
-    return image
+mickey = pygame.image.load("mainclock1.png")
+rightarm = pygame.image.load('rightarm1.png')
+leftarm = pygame.image.load("leftarm.png")
 
-"""_image_librery1 = {}
-def get_image(path):
-    global _image_librery1
-    image =_image_librery1.get(path)
-    if image == None:
-        canon_path = path.replace("\\", os.sep)
-        image = pygame.image.load(canon_path)
-        _image_librery1[path] = image
-    return image"""
+def BlitRotateCenter(surf, image, center, angle):
+    rotate_image = pygame.transform.rotate(image, angle)
+    new_rect = rotate_image.get_rect(center = image.get_rect(center=center).center)
+    surf.blit(rotate_image, new_rect)
 
-current_time = time.localtime()
-minutes = current_time.tm_min
-seconds = current_time.tm_sec
-
-while not done:
+while True:
+    screen.fill(0)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
-    screen.fill((255,255,255))     
-    original_image = get_image("mainclock1.png")
-    #scaled_image = pygame.transform.scale(original_image, (600, 600))  # Change size here
-    screen.blit((original_image), (0, 0)) 
-    original_image1 = get_image("leftarm.png")
-    #scaled_image1 = pygame.transform.scale(original_image1, (100, 100))  # Change size here
-    screen.blit(original_image1, (394, -85)) 
-    pygame.display.flip()
+            exit()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            exit()
+    current_time = datetime.datetime.now()
+    hour_angle = -(current_time.minute * 6)
+    minute_angle = -(current_time.second * 6)
+    
+    screen.fill(WHITE)
+    screen.blit(mickey, (0,0))
+    BlitRotateCenter(screen, rightarm, (x,y), hour_angle)
+    BlitRotateCenter(screen, leftarm, (x,y), minute_angle)
+    
+    pygame.display.update()
     clock.tick(60)

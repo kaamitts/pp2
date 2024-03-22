444a01aeb9276a2072dd3315392bff1d8c11bba7
Kanatbek <atamuratovkanatbek@gmail.com>
2024-03-22 16:43:44 +0500

    Ninth commit

--- lab9/ex3/a1.py
+++ lab9/ex3/a1.py
@@ -1,90 +1,91 @@
 import pygame
-from pygame.locals import *
-
+import os
 pygame.init()
 
-# Создание окна
-screen = pygame.display.set_mode((1080, 800))
+screen = pygame.display.set_mode((1080, 900))
+
 clock = pygame.time.Clock()
 
-# Создание переменных
-radius = 10
-done = False
+RED = (230, 0, 0)
+GREEN = (0, 230, 0)
+BLUE = (0, 0, 230)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+colors = [RED, GREEN, BLUE]
+color = WHITE
 
-# Создание цветов заранее для удобства
-colors = {"RED": (230, 0, 0), "GREEN": (0, 230, 0), "BLUE": (0, 0, 230)}
-color = (255, 255, 255)  # Белый цвет по умолчанию
-
-# Загрузка ластика
-eraser = pygame.image.load("eraser.png")
+eraser = pygame.image.load('eraser.png')
 eraser = pygame.transform.scale(eraser, (70, 70))
 
-# Функция рисования квадратов цветов и кнопки ластика
-def draw_color_buttons():
-    for i, (color_name, color_value) in enumerate(colors.items()):
-        pygame.draw.rect(screen, color_value, (i * 40, 0, 40, 40))
-    pygame.draw.rect(screen, (255, 255, 255), (0, 0, 40, 60), 4)  # Белый
-    pygame.draw.rect(screen, (255, 255, 255), (40, 0, 40, 40), 4)  # Ластика
+def draw_rect(index):
+    pygame.draw.rect(screen, colors[index], (index*40, 0, 40, 40))
 
-# Функция выбора цвета
-def color_pick(x, y):
-    for i, (color_name, color_value) in enumerate(colors.items()):
-        if i * 40 < x < (i + 1) * 40 and 0 < y < 40:
-            return color_value
-    if 0 < x < 40 and 0 < y < 60:
-        return (255, 255, 255)  # Белый
-    if 40 < x < 80 and 0 < y < 40:
-        return "eraser"
+def pick_color():
+    click = pygame.mouse.get_pressed()
+    x, y = pygame.mouse.get_pos()
+    if click[0]:
+        if 0<=x<=40 and 0<=y<=40:
+            return RED
+        elif 40<x<=80 and 0<=y<=40:
+            return GREEN
+        elif 80<x<=120 and 0<=y<=40:
+            return BLUE
+        elif 1010<=x<=1080 and 0<=y<=40:
+            return BLACK
     return color
 
-# Функция рисования фигур
-def draw_shape(shape, x, y):
-    if shape == "circle":
-        pygame.draw.circle(screen, color, (x, y), 20, 4)
-    elif shape == "rect":
-        pygame.draw.rect(screen, color, (x, y, 40, 60), 4)
-    elif shape == "square":
-        pygame.draw.rect(screen, color, (x, y, 40, 40), 4)
-    elif shape == "right triangle":
-        pygame.draw.polygon(screen, color, [(x, y), (x + 40, y), (x, y + 60)], 4)
-    elif shape == "equilateral triangle":
-        pygame.draw.polygon(screen, color, [(x + 20, y), (x, y + 40), (x + 40, y + 40)], 4)
-    elif shape == "rhombus":
-        pygame.draw.polygon(screen, color, [(x, y + 30), (x + 20, y), (x + 40, y + 30), (x + 20, y + 60)], 4)
+def painting(color):
+    click = pygame.mouse.get_pressed()
+    x, y = pygame.mouse.get_pos()
+    if click[0] and not (0<=x<=400 and 0<=y<=90):
+        if mode == 'circle':
+            pygame.draw.circle(screen, color, (x, y), 27)
+        if mode == 'rect':
+            pygame.draw.rect(screen, color, (x, y, 40, 40), 4)
+        if mode == 'right_triangle':
+            pygame.draw.polygon(screen, color, ((x, y), (x, y+40), (x+40, y+40)), 3)
+        if mode == 'equal_triangle':
+            pygame.draw.polygon(screen, color, ((x,y), (x+20, y-40), (x+40, y)))
+        if mode == 'rhomb':
+            pygame.draw.polygon(screen, color, ((x, y), (x+20, y-20), (x+40, y), (x+20, y+20)))
 
-while not done:
+mode = 'circle'
+
+while True:
+
+
     for event in pygame.event.get():
-        if event.type == QUIT:
-            done = True
-        if event.type == KEYDOWN and event.key == K_ESCAPE:
-            done = True
-        if event.type == MOUSEBUTTONDOWN:
-            x, y = pygame.mouse.get_pos()
-            color = color_pick(x, y)
+        if event.type == pygame.QUIT:
+            pygame.quit()
 
-    # Отрисовка квадратов цветов и кнопки ластика
-    draw_color_buttons()
+    
+    for i in range(len(colors)):
+        draw_rect(i)
 
-    # Выбор фигуры для рисования и ее отрисовка
-    keys = pygame.key.get_pressed()
-    if keys[K_r]:
-        draw_shape("rect", x, y)
-    elif keys[K_c]:
-        draw_shape("circle", x, y)
-    elif keys[K_s]:
-        draw_shape("square", x, y)
-    elif keys[K_t]:
-        draw_shape("right triangle", x, y)
-    elif keys[K_e]:
-        draw_shape("equilateral triangle", x, y)
-    elif keys[K_h]:
-        draw_shape("rhombus", x, y)
-    elif color == "eraser":
-        screen.blit(eraser, (0, 0))
-    else:
-        pygame.draw.circle(screen, color, pygame.mouse.get_pos(), radius)
+    screen.blit(eraser, (1010, 0))
+    rect = pygame.draw.rect(screen, WHITE, (130, 0, 40, 40), 3)
+    circle = pygame.draw.circle(screen, WHITE, (197, 20), 23, 3)
+    right = pygame.draw.polygon(screen, WHITE, ((230, 0), (230, 40), (270, 40)), 3)
+    equal = pygame.draw.polygon(screen, WHITE, ((280, 40), (300, 0), (320, 40)), 3)
+    rhomb = pygame.draw.polygon(screen, WHITE, ((330, 20), (350,0), (370, 20), (350, 40)), 3)
 
-    pygame.display.flip()
-    clock.tick(60)
+    pos = pygame.mouse.get_pos()
+    print(mode)
+    if rect.collidepoint(pos):
+        mode = "rect"
+    if circle.collidepoint(pos):
+        mode = "circle"
+    if right.collidepoint(pos):
+        mode = 'right_triangle'
+    if equal.collidepoint(pos):
+        mode = 'equal_triangle'
+    if rhomb.collidepoint(pos):
+        mode = 'rhomb'
 
-pygame.quit()
+
+    color = pick_color()
+    painting(color)
+
+
+    clock.tick(370)
+    pygame.display.update()
